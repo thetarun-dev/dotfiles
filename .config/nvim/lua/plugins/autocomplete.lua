94e61bac4fdf731b4a4c90055aec68d20ca715a8
Tarun Bhandari <tarunbhandari.work@gmail.com>
2026-08-05 19:07:21 +0530

    modify
    remove the default comments and settings

--- .config/nvim/lua/plugins/autocomplete.lua
+++ .config/nvim/lua/plugins/autocomplete.lua
@@ -81,9 +81,9 @@ return { -- Autocompletion
       -- No, but seriously. Please read `:help ins-completion`, it is really good!
       mapping = cmp.mapping.preset.insert {
         -- Select the [n]ext item
-        ['<C-n>'] = cmp.mapping.select_next_item(),
+        ['<Tab>'] = cmp.mapping.select_next_item(),
         -- Select the [p]revious item
-        ['<C-p>'] = cmp.mapping.select_prev_item(),
+        ['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
         -- Scroll the documentation window [b]ack / [f]orward
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -92,7 +92,7 @@ return { -- Autocompletion
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
         --  This will expand snippets if the LSP sent a snippet.
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<CR>'] = cmp.mapping.confirm { select = true },
 
         -- If you prefer more traditional completion keymaps,
         -- you can uncomment the following lines
@@ -173,3 +173,5 @@ return { -- Autocompletion
     }
   end,
 }
+
+
--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,15 +3,7 @@ return {
   dependencies = {
     -- Automatically install LSPs and related tools to stdpath for Neovim
     { 'mason-org/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
-    -- mason-lspconfig:
-    -- - Bridges the gap between LSP config names (e.g. "lua_ls") and actual Mason package names (e.g. "lua-language-server").
-    -- - Used here only to allow specifying language servers by their LSP name (like "lua_ls") in `ensure_installed`.
-    -- - It does not auto-configure servers — we use vim.lsp.config() + vim.lsp.enable() explicitly for full control.
     'mason-org/mason-lspconfig.nvim',
-    -- mason-tool-installer:
-    -- - Installs LSPs, linters, formatters, etc. by their Mason package name.
-    -- - We use it to ensure all desired tools are present.
-    -- - The `ensure_installed` list works with mason-lspconfig to resolve LSP names like "lua_ls".
     'WhoIsSethDaniel/mason-tool-installer.nvim',
 
     -- Useful status updates for LSP.
@@ -20,7 +12,7 @@ return {
       opts = {
         notification = {
           window = {
-            winblend = 0, -- Background color opacity in the notification window
+            winblend = 0,
           },
         },
       },
@@ -115,60 +107,10 @@ return {
       end,
     })
 
-    -- LSP servers and clients are able to communicate to each other what features they support.
-    -- By default, Neovim doesn't support everything that is in the LSP specification.
-    -- When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
-    -- So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
     local capabilities = vim.lsp.protocol.make_client_capabilities()
     capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
-    -- Enable the following language servers
-    --
-    -- Add any additional override configuration in the following tables. Available keys are:
-    -- - cmd (table): Override the default command used to start the server
-    -- - filetypes (table): Override the default list of associated filetypes for the server
-    -- - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
-    -- - settings (table): Override the default settings passed when initializing the server.
-    local servers = {
-      pylsp = {
-        settings = {
-          pylsp = {
-            plugins = {
-              pyflakes = { enabled = false },
-              pycodestyle = { enabled = false },
-              autopep8 = { enabled = false },
-              yapf = { enabled = false },
-              mccabe = { enabled = false },
-              pylsp_mypy = { enabled = false },
-              pylsp_black = { enabled = false },
-              pylsp_isort = { enabled = false },
-            },
-          },
-        },
-      },
-      html = { filetypes = { 'html', 'twig', 'hbs' } },
-      lua_ls = {
-        settings = {
-          Lua = {
-            completion = {
-              callSnippet = 'Replace',
-            },
-            runtime = { version = 'LuaJIT' },
-            workspace = {
-              checkThirdParty = false,
-              library = vim.api.nvim_get_runtime_file('', true),
-            },
-            diagnostics = {
-              globals = { 'vim' },
-              disable = { 'missing-fields' },
-            },
-            format = {
-              enable = false,
-            },
-          },
-        },
-      },
-    }
+    local servers = {}
 
     -- Ensure the servers and tools above are installed
     local ensure_installed = vim.tbl_keys(servers or {})
@@ -178,10 +120,6 @@ return {
     require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
     for server, cfg in pairs(servers) do
-      -- For each LSP server (cfg), we merge:
-      -- 1. A fresh empty table (to avoid mutating capabilities globally)
-      -- 2. Your capabilities object with Neovim + cmp features
-      -- 3. Any server-specific cfg.capabilities if defined in `servers`
       cfg.capabilities = vim.tbl_deep_extend('force', {}, capabilities, cfg.capabilities or {})
 
       vim.lsp.config(server, cfg)
